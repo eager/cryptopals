cfd61bb311fa7b206896b8073782f2d43f4db276
Christian Eager <ceager@gmail.com>
2023-01-14 11:40:07 -0600

    set 1: challenges 1 + 2

---
+++ set1/2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/hex"
+	"errors"
+	"log"
+)
+
+func main() {
+
+	h1, e1 := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	h2, e2 := hex.DecodeString("686974207468652062756c6c277320657965")
+
+	if e1 != nil || e2 != nil {
+		panic("failed to decode")
+	}
+
+	actual, err := xor(h1, h2)
+
+	if err != nil {
+		panic("failed to xor")
+	}
+
+	expected := "746865206b696420646f6e277420706c6179"
+
+	log.Printf("actual == expected: %v", hex.EncodeToString(actual) == expected)
+
+}
+
+func xor(b1, b2 []byte) ([]byte, error) {
+
+	if len(b1) != len(b2) {
+		return nil, errors.New("slices of different lengths")
+	}
+
+	result := make([]byte, 0)
+
+	for i, _ := range b1 {
+		result = append(result, b1[i] ^ b2[i])
+	}
+
+	return result, nil
+
+}
---
+++ set1/go.mod
@@ -0,0 +1,3 @@
+module github.com/eager/cryptopals/set1
+
+go 1.19
