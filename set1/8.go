37d6ddec9310ccb8a4d2ba8e5fb8d4810cce84ae
Christian Eager <ceager@gmail.com>
2023-01-15 20:34:51 -0600

    set 1: challenge 8

---
+++ set1/8.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	input, err := os.ReadFile("./8.txt")
+	if err != nil {
+		panic("failed to read input")
+	}
+
+	bestRepeatedBlocks := 0
+	blockSize := 16
+	aesCipher := ""
+
+	for _, line := range strings.Split(string(input), "\n") {
+		cipher, err := hex.DecodeString(line)
+		if err != nil {
+			panic("could not decode line")
+		}
+
+		repeatedBlocks := 0
+
+		for len(cipher)-blockSize > 0 {
+			block := cipher[0:blockSize]
+			for c := cipher; len(c) > blockSize; c = c[blockSize:] {
+
+				if bytes.Compare(block, c[0:blockSize]) == 0 {
+					repeatedBlocks = repeatedBlocks + 1
+				}
+
+			}
+			cipher = cipher[blockSize:]
+		}
+		if repeatedBlocks > bestRepeatedBlocks {
+			bestRepeatedBlocks = repeatedBlocks
+			aesCipher = line
+		}
+	}
+
+	log.Printf("repeated blocks: %d", bestRepeatedBlocks)
+	log.Print(aesCipher)
+}
